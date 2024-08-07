e493672e03cdc249c520b069ae6c871d5f0f05bd
Waleed0260 <muhammadwaleedahsan43@gmail.com>
2024-08-07 06:31:45 -0700

    push

--- components/home/Consultation.tsx
+++ components/home/Consultation.tsx
@@ -10,19 +10,20 @@ const Consultation = () => {
 
         <p className='font-bold text-2xl heading'>READY TO GROW YOUR BUSINESS</p>
         <div className='flex flex-row m-6 items-center gap-4'>
-            <div className='w-[50%]'>
+            <div className='w-[30%]'>
                 <Image src={chart} alt='chart' className='w-full h-full overflow-hidden'/>
             </div>
-            <div className=' flex flex-col w-[50%] m-4 bg'>
-                <form action="" className='flex flex-col m-2 text-white'>
+            <div className=' flex flex-col w-[65%] m-4'>
+                <p className='font-bold text-xl mid-heading'>Get a Free consultation</p>
+                <form action="" className='flex flex-col p-5 bg-[#F5F5F5]'>
                     <label className='mt-4' htmlFor="name">Full Name</label>
-                    <input type="text" name="name" id="" placeholder='Name'  className='p-4 bg-transparent border-[1px] border-[#F5F5F5]'/>
+                    <input type="text" name="name" id="" placeholder='Name'  className='p-4 bg-white border-[1px] border-[#F5F5F5]'/>
                     <label className='mt-4' htmlFor="email">Email</label>
-                    <input type="text" name='email' placeholder='Enter Email'  className='p-4 bg-transparent border-[1px] border-[#F5F5F5]'/>
+                    <input type="text" name='email' placeholder='Enter Email'  className='p-4 bg-white border-[1px] border-[#F5F5F5]'/>
                     <label className='mt-4' htmlFor="email">Subject</label>
-                    <input type="text" name='email' placeholder='Enter Subject'  className='p-4 bg-transparent border-[1px] border-[#F5F5F5] '/> 
+                    <input type="text" name='email' placeholder='Enter Subject'  className='p-4 bg-white border-[1px] border-[#F5F5F5] '/> 
                     <label className='mt-4' htmlFor="email">Message</label>
-                    <input type="text" name='email' placeholder='Enter Email'  className='p-4 bg-transparent border-[1px] border-[#F5F5F5]'/>
+                    <input type="text" name='email' placeholder='Enter Email'  className='p-4 bg-white border-[1px] border-[#F5F5F5]'/>
                     <Button className='bg-white text-black mt-4 rounded-[100px]'>SUBMIT</Button>
                 </form>
             </div>
--- components/home/Technologies.tsx
+++ components/home/Technologies.tsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import google from "../../public/images/google.png"
 import Image from 'next/image'
+import adobe from "../../public/images/adobe.png"
+import meta from "../../public/images/meta.png"
+import shopify from "../../public/images/Shopify.png"
+import googleads from "../../public/images/googleads.png"
+import hubspot from "../../public/images/hubspot.png"
+import kyaviyo from "../../public/images/kyaviyo.png"
+import magento from "../../public/images/magento.png"
+import react from "../../public/images/react.png"
+import woocommerce from "../../public/images/woocommerce.png"
+import wordpress from "../../public/images/wordpress.png"
 
 const Technologies = () => {
   return (
@@ -8,18 +18,18 @@ const Technologies = () => {
         <p className='font-bold text-2xl heading'>Experts  in Best Class Technology Platforms</p>
         <div className='w-[80%] flex flex-col items-center justify-center'>
             <div className="grid grid-cols-6 gap-3">
-                <Image src={google} alt="" className='w-[200px] h-[150px]'/>
-                <Image src={google} alt="" className='w-[200px] h-[150px]'/>
-                <Image src={google} alt="" className='w-[200px] h-[150px]'/>
-                <Image src={google} alt="" className='w-[200px] h-[150px]'/>
-                <Image src={google} alt="" className='w-[200px] h-[150px]'/>
-                <Image src={google} alt="" className='w-[200px] h-[150px]'/>
+                <Image src={adobe} alt="" className='w-[150px] h-[100px]'/>
+                <Image src={meta} alt="" className='w-[150px] h-[100px]'/>
+                <Image src={shopify} alt="" className='w-[150px] h-[100px]'/>
+                <Image src={googleads} alt="" className='w-[100px] h-[100px]'/>
+                <Image src={hubspot} alt="" className='w-[150px] h-[100px]'/>
+                <Image src={kyaviyo} alt="" className='w-[150px] h-[100px]'/>
             </div>
-            <div className="grid grid-cols-4">
-            <Image src={google} alt="" className='w-[200px] h-[150px]'/>
-                <Image src={google} alt="" className='w-[200px] h-[150px]'/>
-                <Image src={google} alt="" className='w-[200px] h-[150px]'/>
-                <Image src={google} alt="" className='w-[200px] h-[150px]'/>
+            <div className="grid grid-cols-4 gap-3">
+            <Image src={magento} alt="" className='w-[150px] h-[100px]'/>
+                <Image src={woocommerce} alt="" className='w-[150px] h-[100px]'/>
+                <Image src={react} alt="" className='w-[150px] h-[100px]'/>
+                <Image src={wordpress} alt="" className='w-[150px] h-[100px]'/>
             </div>
         </div>
     </div>
