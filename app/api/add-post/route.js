dad828f50caa605d8391738376947dd60973c135
Waleed0260 <muhammadwaleedahsan43@gmail.com>
2024-08-29 04:46:36 -0700

    done no bugs

--- app/api/add-post/route.js
+++ app/api/add-post/route.js
@@ -3,11 +3,11 @@ import { NextResponse } from "next/server";
 
 export async function POST(request) {
     const res = await request.json();
-    const {name, email} = res;
+    const {name, email, subject, message} = res;
     console.log({res});
     await prisma.User.create({
         data:{
-            name, email
+            name, email, subject, message
         }
     })
     return NextResponse.json({data:res})
